ed11467d2c2cf0ae97cca18aeb3d5ced4d8e1902
Ji__ho <cale.jiho@gmail.com>
2024-02-24 21:38:57 +0900

    getStaticProps로 파일 접근하기(nodeJS)

--- section6/pages/index.js
+++ section6/pages/index.js
@@ -1,9 +1,28 @@
+import path from "path";
+import fs from "fs/promises";
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products,
+    },
+  };
+}
+
 function HomePage(props) {
+  /*
+  의문점!
+  props를 상위 컴포넌트로부터 전달받는 경우에는 getStaticProps와 상위 컴포넌트 중에 어떤걸 사용하는 거지?
+   */
+  const { products } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
 }
